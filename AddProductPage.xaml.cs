f48a96605264848fdbbda51815e984f0ebc7dc1d
gregorchik <tenyakov05.mail.ru>
2024-11-28 02:53:31 +0300

    first commit

---
+++ AddProductPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class AddProductPage : ContentPage
+{
+	public AddProductPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ UpdateProductPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class UpdateProductPage : ContentPage
+{
+	public UpdateProductPage()
+	{
+		InitializeComponent();
+	}
+}
